fdea228cb4ab72d96d03f30e54547845803c7fd8
adoka <adokanchi@gmail.com>
2024-12-10 09:55:51 -0800

    A

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -6,6 +6,8 @@ Name: Tony Dokanchi
 | Dec 4   | 12:40-1:40  |  Discussions about potential approaches---something about detecting common sequences of characters (" the " and things like that), potentially by taking a sample of the beginning of the text, and taking advantage of that redundancy? The exact approach is still in question. |
 | Dec 6   |  9:15-9:45  | Built the code to store the first 100 words (without spaces) in an array. Created an int-char map with 75 characters used commonly in text. Using 7-bit codes with 128 possibilities, that gives space for 53 stored words. If 8-bit codes were used, this could increase to 181. |
 | Dec 6   | 10:00-10:40 |     Built a mix of code and pseudocode that would apply the compression. It seems like it would be really really slow to compress, but I don't see any other way to do something like this. It's also really (probably unnecessarily) complicated, but I don't see a simpler way. |
+| Dec 10  | 8:45-9:15am |                                                                                                                                                                                                                                              In-class discussion about edge cases |
+| Dec 10  | 9:20-9:55am |                                                                Began coding LZW Compression. I think what I have should work in theory (aside from the edge case), but I'm still in the process of verifying it. I'm pretty much just translating the pseudocode from the slides. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -28,104 +28,37 @@
  *  @author Zach Blick, Tony Dokanchi
  */
 public class TextCompressor {
-
-
-    final static int BLOCK_LENGTH = 8;
-    final static int NUM_CHARS = 75;
-    final static int NUM_STORED_WORDS = (1 << BLOCK_LENGTH) - NUM_CHARS;
-    static String[] firstWords;
-
+    private static final int EOF = 256;
     private static void compress() {
+        TST tree = new TST();
+        int code = 257;
 
-        firstWords = new String[NUM_STORED_WORDS];
-
-        for (int i = 0; i < NUM_STORED_WORDS; i++) {
-            String word = "";
-            while (true) {
-                char nextChar = BinaryStdIn.readChar();
-                if (nextChar == ' ') {
-                    firstWords[i] = word;
-                    break;
-                }
-                else {
-                    word += nextChar;
-                }
-            }
+        // Add all codes from extended ASCII
+        for (int i = 0; i < 255; i++) {
+            tree.insert("" + (char) i, i);
         }
 
-        int longestLen = 0;
-        for (int i = 0; i < NUM_STORED_WORDS; i++) if (firstWords[i].length() > longestLen) longestLen = firstWords[i].length();
+        String text = BinaryStdIn.readString();
 
-        // Fill in with longestLen chars from BinaryStdIn so we can check is any of the stored words are in the text.
-        String in = "";
-        while (in.length() < longestLen) {
-            in += BinaryStdIn.readChar();
+        int index = 0;
+        int len = text.length();
+        while (index < len) {
+            String prefix = tree.getLongestPrefix(text.substring(index));
+            BinaryStdOut.write(tree.lookup(prefix));
+            if (index + prefix.length() < text.length()) {
+                tree.insert(prefix + text.charAt(index + prefix.length()), code);
+            }
+            code++;
+            index += prefix.length();
         }
 
-        while (!in.isEmpty()) {
-            boolean found = false;
-            for (String word : firstWords) {
-                if (in.substring(0, word.length()).equals(word)) {
-                    BinaryStdOut.write(wordToCode(word));
-                    found = true;
-                }
-            }
-            if (!found) {
-                BinaryStdOut.write(charToCode(in.charAt(0)));
-                in = in.substring(1);
-            }
-
-            if (!BinaryStdIn.isEmpty()) {
-                while (in.length() < longestLen) {
-                    in += BinaryStdIn.readChar();
-                }
-            }
-        }
-
-
-
+        BinaryStdOut.write(EOF);
         BinaryStdOut.close();
     }
 
-    // Takes in a word from firstWords and outputs the code
-    private static int wordToCode(String word) {
-        for (int i = 0; i < NUM_STORED_WORDS; i++) {
-            if (word.equals(firstWords[i])) {
-                return i + NUM_CHARS;
-            }
-        }
-        return -1;
-    }
-
-    private static int charToCode(char c) {
-        // TODO: Add the map
-        return -1;
-    }
-
-    private static String codeToWord(int code) {
-        return firstWords[code - NUM_CHARS];
-    }
-
-    private static String codeToChar(int code) {
-        // TODO: Add the map
-        return null;
-    }
-
     private static void expand() {
-
-        while (!BinaryStdIn.isEmpty()) {
-            int code = BinaryStdIn.readInt(BLOCK_LENGTH);
-            if (code < NUM_CHARS) {
-                BinaryStdOut.write(codeToChar(code));
-            }
-            else {
-                BinaryStdOut.write(codeToWord(code));
-            }
-        }
-        // TODO: Complete the expand() method
-
-
-
+        String text = BinaryStdIn.readString();
+        BinaryStdOut.write(text);
         BinaryStdOut.close();
     }
 
